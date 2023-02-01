c1854b25730d9e7a472b7005e577fb8bb6363206
Maanav Khaitan <maanav@berkeley.edu>
2023-01-31 16:43:23 -0800

    Changed Google Form link

--- src/Components/ChatButton.js
+++ src/Components/ChatButton.js
@@ -5,7 +5,7 @@ function ChatButton() {
   return (
     <Center>
         <Flex justifyContent='center' flexWrap='wrap'>
-        <a target="_blank" rel="noreferrer" href="https://met.berkeley.edu/about-met/contact/">
+        <a target="_blank" rel="noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSd9a_JT-o4DK9tB7yMLNylKRYc-KL3MMFyDfFdOcy-N9hhiJQ/viewform">
           <Button
               bg='#3B7EA1'
               color='white'
