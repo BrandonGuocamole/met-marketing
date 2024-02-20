9226eb76f22059620b9d32c9209e683e67d7a36d
Maanav Khaitan <maanav@berkeley.edu>
2024-02-20 15:54:29 -0800

    Update internships pic

--- src/Components/Internships.js
+++ src/Components/Internships.js
@@ -10,7 +10,7 @@ function Internships () {
                 </Box>
             </Center>
             <Center>
-                <Image src='https://i.imgur.com/jW0M5zn.jpg'/>
+                <Image src='https://i.imgur.com/lGn5ROa.jpeg'/>
             </Center>
         </Box>
     )
