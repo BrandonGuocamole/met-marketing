e7ac0d1479a4fb9a285662eca57bda6983e0f6a6
Maanav Khaitan <me@maanavkhaitan.com>
2025-01-22 16:24:34 -0500

    update links

--- src/Components/MediaProfile.js
+++ src/Components/MediaProfile.js
@@ -14,15 +14,15 @@ function MediaProfile() {
             </Center>
             <Center>
                 <Flex maxWidth='80vw'>
-                    <a add target="_blank" rel="noreferrer" href='https://drive.google.com/file/d/1kflVMYMWUZ-En0IDShjIqAw3icW801xk/view?usp=sharing'>
-                        <Image w='300px' src='https://media.insider.com/public/assets/BI/US/logos/logos-page/BI_blue_background_vertical.png' />
+                    <a add target="_blank" rel="noreferrer" href='https://drive.google.com/file/d/1KkWso1uk4h9kzSWO0wcu4EImIToWgMVH/view?usp=sharing'>
+                        <Image w='300px' src='https://logos-world.net/wp-content/uploads/2022/05/Business-Insider-Logo.png' />
                     </a>
                     <Box w='10vw' />
-                    <a add target="_blank" rel="noreferrer" href='https://drive.google.com/file/d/1lm3ECmWC4DNevA-seCZ8ymMa4rukWhap/view?usp=sharing' >
+                    <a add target="_blank" rel="noreferrer" href='https://drive.google.com/file/d/1w-5gcqm_JLRY-z_95EYDNfNUdT1SiRIJ/view?usp=sharing' >
                         <Image w='300px' src='https://www.mixt.com/wp-content/uploads/2016/04/The-Mercury-News-logo.png' />
                     </a>
                     <Box w='10vw' />
-                    <a add target="_blank" rel="noreferrer" href='https://drive.google.com/file/d/19y1Rm6hYkkQTIypwuAAnBAGXK6G5mA-3/view?usp=sharing'>
+                    <a add target="_blank" rel="noreferrer" href='https://drive.google.com/file/d/1woeWdk8ism9HZz4xNFBepMw-m8BLisQ2/view?usp=sharing'>
                         <Image w='300px' src='https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iVy9T_QYLR68/v5/-1x-1.jpg' />
                     </a>
                 </Flex>
